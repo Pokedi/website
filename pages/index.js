ceb1fc17cd6e11b5369486f86abff5ce268c1a5d
DittoDuckPenguin <18628188+DittoDuckPenguin@users.noreply.github.com>
2023-10-05 00:24:09 +0330

    🐞 fix: Jumpy add pokedi button

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
                   <a href="https://discord.gg/BUnZdjY" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Discord</a>
                 </li>
                 <li>
-                  <a href="https://discord.com/api/oauth2/authorize?client_id=716293342740348948&permissions=379968&scope=applications.commands%20bot" className="w-fit block text-gray-900 rounded-xl hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-black light:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent bg-white px-2 py-1 font-normal">Add Pokédi</a>
+                  <a href="https://discord.com/api/oauth2/authorize?client_id=716293342740348948&permissions=379968&scope=applications.commands%20bot" className="w-fit block text-gray-900 rounded-xl hover:bg-gray-100 md:border-0 md:hover:text-blue-700 dark:text-black light:text-white md:dark:hover:text-blue-500 dark:hover:text-white bg-white px-2 py-1 font-normal hover:-translate-y-1 transition-transform">Add Pokédi</a>
                 </li>
               </ul>
             </div>
