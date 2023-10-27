cdf94b785b9bf69213f0edc11f8cea8298a1ab03
DittoDuckPenguin <18628188+DittoDuckPenguin@users.noreply.github.com>
2023-10-27 23:27:03 +0330

    🖥️ wip: Testing API handler

---
+++ pages/api/topgg.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        console.log(req.query, req.body);
+        // Process a POST request
+        res.json({ success: true });
+    } else {
+        // Handle any other HTTP method
+        res.json({ success: false });
+    }
+}
