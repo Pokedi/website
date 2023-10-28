826be1e8494d144112c965f8c4aeb3f5bb38e06c
DittoDuckPenguin <18628188+DittoDuckPenguin@users.noreply.github.com>
2023-10-28 09:25:21 +0330

    🖥️ wip: Let's test this out

--- pages/api/topgg.ts
+++ pages/api/topgg.ts
@@ -1,9 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import TopGG from "@top-gg/sdk";
+
+const webhook = new TopGG.Webhook(process.env.TOPGG);
+
+export default function handler(req: NextApiRequest, res: NextApiResponse, next: NextApiHandler) {
     if (req.method === 'POST') {
         console.log(req.query, req.body);
         // Process a POST request
-        res.json({ success: true });
+        // res.json({ success: true });
+        webhook.listener((vote)=>{console.log(vote, "VOTE")})(req, res, next);
     } else {
         console.log(req.query, req.body);
         // Handle any other HTTP method
