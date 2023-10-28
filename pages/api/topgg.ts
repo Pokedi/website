5d23924d0d16a73ed6b4d215604de79a4bc5ef67
DittoDuckPenguin <18628188+DittoDuckPenguin@users.noreply.github.com>
2023-10-28 09:14:20 +0330

    🖥️ wip: Adding sharp

--- pages/api/topgg.ts
+++ pages/api/topgg.ts
@@ -1,11 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         console.log(req.query, req.body);
         // Process a POST request
         res.json({ success: true });
     } else {
+        console.log(req.query, req.body);
         // Handle any other HTTP method
         res.json({ success: false });
     }
