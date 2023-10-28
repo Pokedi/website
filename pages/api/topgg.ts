17dbc1df87859d5cc04a2043c5f00b868bdbcd20
DittoDuckPenguin <18628188+DittoDuckPenguin@users.noreply.github.com>
2023-10-28 09:26:39 +0330

    🐞 fix: Unifying all request methods

--- pages/api/topgg.ts
+++ pages/api/topgg.ts
@@ -4,14 +4,14 @@ import TopGG from "@top-gg/sdk";
 const webhook = new TopGG.Webhook(process.env.TOPGG);
 
 export default function handler(req: NextApiRequest, res: NextApiResponse, next: NextApiHandler) {
-    if (req.method === 'POST') {
-        console.log(req.query, req.body);
+    // if (req.method === 'POST') {
+    //     console.log(req.query, req.body);
         // Process a POST request
         // res.json({ success: true });
         webhook.listener((vote)=>{console.log(vote, "VOTE")})(req, res, next);
-    } else {
-        console.log(req.query, req.body);
-        // Handle any other HTTP method
-        res.json({ success: false });
-    }
+    // } else {
+    //     console.log(req.query, req.body);
+    //     // Handle any other HTTP method
+    //     res.json({ success: false });
+    // }
 }
