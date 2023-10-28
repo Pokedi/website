169d972bdb979a334ba9856a8dc2b5a2d46b53ed
DittoDuckPenguin <18628188+DittoDuckPenguin@users.noreply.github.com>
2023-10-28 09:31:05 +0330

    🐞 fix: Attempting another fix

--- pages/api/topgg.ts
+++ pages/api/topgg.ts
@@ -1,17 +1,17 @@
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
-import TopGG from "@top-gg/sdk";
+import { Webhook } from "@top-gg/sdk";
 
-const webhook = new TopGG.Webhook(process.env.TOPGG);
+const webhook = new Webhook(process.env.TOPGG);
 
 export default function handler(req: NextApiRequest, res: NextApiResponse, next: NextApiHandler) {
-    // if (req.method === 'POST') {
-    console.log(req.query, req.body);
-    // Process a POST request
-    // res.json({ success: true });
-    webhook.listener((vote) => { console.log(vote, "VOTE") })(req, res, next);
-    // } else {
-    //     console.log(req.query, req.body);
-    //     // Handle any other HTTP method
-    //     res.json({ success: false });
-    // }
+    if (req.method === 'POST') {
+        console.log(req.query, req.body);
+        // Process a POST request
+        // res.json({ success: true });
+        webhook.listener((vote) => { console.log(vote, "VOTE") })(req, res, next);
+        // } else {
+        //     console.log(req.query, req.body);
+        //     // Handle any other HTTP method
+        //     res.json({ success: false });
+    }
 }
