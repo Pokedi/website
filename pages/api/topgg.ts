5eb6432ab2c33cc8daecf5076aabe75a6a22f7c7
DittoDuckPenguin <18628188+DittoDuckPenguin@users.noreply.github.com>
2023-10-28 10:46:22 +0330

    🐞 fix: Attempting sendStatus fix

--- pages/api/topgg.ts
+++ pages/api/topgg.ts
@@ -3,11 +3,12 @@ import { Webhook } from "@top-gg/sdk";
 
 const webhook = new Webhook(process.env.TOPGG);
 
-export default function handler(req: NextApiRequest, res: NextApiResponse, next: NextApiHandler) {
+export default function handler(req: NextApiRequest, res: any, next: NextApiHandler) {
     if (req.method === 'POST') {
         console.log(req.query, req.body);
         // Process a POST request
         // res.json({ success: true });
+        res.sendStatus = res.status;
         webhook.listener((vote) => { console.log(vote, "VOTE") })(req, res, next);
     } else {
         console.log(req.query, req.body);
