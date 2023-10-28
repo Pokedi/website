12269aeb99defb0b45209df04e5a9cac355af1ee
DittoDuckPenguin <18628188+DittoDuckPenguin@users.noreply.github.com>
2023-10-28 09:28:27 +0330

    🐞 fix: Adding a console log just in case

--- pages/api/topgg.ts
+++ pages/api/topgg.ts
@@ -5,10 +5,10 @@ const webhook = new TopGG.Webhook(process.env.TOPGG);
 
 export default function handler(req: NextApiRequest, res: NextApiResponse, next: NextApiHandler) {
     // if (req.method === 'POST') {
-    //     console.log(req.query, req.body);
-        // Process a POST request
-        // res.json({ success: true });
-        webhook.listener((vote)=>{console.log(vote, "VOTE")})(req, res, next);
+    console.log(req.query, req.body);
+    // Process a POST request
+    // res.json({ success: true });
+    webhook.listener((vote) => { console.log(vote, "VOTE") })(req, res, next);
     // } else {
     //     console.log(req.query, req.body);
     //     // Handle any other HTTP method
