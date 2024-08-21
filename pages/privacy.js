1264db4a23dd7a9b509979ae5d0e8f90c0a1d8c1
DittoDuckPenguin <18628188+DittoDuckPenguin@users.noreply.github.com>
2024-08-21 22:08:15 +0330

    🖥️ wip: Updating Privacy Policy

--- pages/privacy.js
+++ pages/privacy.js
@@ -24,12 +24,23 @@ export default function Privacy() {
 
                     {/* Head */}
                     <h1 className="text-3xl font-bold mt-6">Privacy Policy</h1>
-                    <b>
-                        Effective Date: August 21, 2024
-                    </b>
-                    <h2 className="text-xl font-bold mt-6">1. Acceptance of Terms</h2>
-                    <p>Your Souls belong to Anthony if you ever interact with the bot, accidentally or intentionally.</p>
-                    
+                    <p>
+                        <strong>Effective Date:</strong> August 21, 2024</p>
+                    <p>Privacy is very important for us at Pokédi. Below is how we handle your data.</p>
+                    <h2 className="text-xl font-bold mt-6">1. Data Collection and Use</h2>
+                    <p>The following information is stored for the proper operation of the Services:</p>
+                    <ul className=" list-disc ms-5 ps-5">
+                        <li>Discord user, server, and channel names and identifiers, to associate you, your channels, and your servers, with your configuration and assets within the Services;</li>
+                        <li>user-supplied content, including, but not limited to, text for nicknames, bot prefixes, location configuration; and</li>
+                        <li>data regarding interactions you have made with the Services, including commands executed, time of execution, Discord server and channel of execution, and any arguments passed to the command.</li>
+                    </ul>
+                    <p>We retain the information collected for as long as is necessary to properly provide you with access to the Services. Data is removed when the reason for its collection is no longer applicable.</p>
+                    <h2 className="text-xl font-bold mt-6">2. Third Parties</h2>
+                    <p>We do not provide any collected data to third parties for reasons beyond maintaining the proper operation of the Services.
+                        Our Services may link to external sites not operated by us. We do not have any control over the policies of these sites.</p>
+                    <h2 className="text-xl font-bold mt-6">3. Contacting Us</h2>
+                    <p>If you have any questions about this Privacy Policy, please contact us in our Discord support server <a href="https://discord.gg/BUnZdjY">https://discord.gg/BUnZdjY</a>.</p>
+
                 </section>
 
             </main>
